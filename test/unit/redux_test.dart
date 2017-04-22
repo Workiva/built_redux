0176881b73adbc97d04e6546ab775e8240b384f0
David Marne <davemarne@gmail.com>
2017-04-22 12:14:32 -0600

    test multiple middleware

--- test/unit/redux_test.dart
+++ test/unit/redux_test.dart
@@ -9,23 +9,30 @@ main() {
   group('redux', () {
     Store<BaseCounter, BaseCounterActions> store;
 
-    setUp(() {
+    setup({int numMiddleware: 1}) {
       var actions = new BaseCounterActions();
       var defaultValue = new BaseCounter((BaseCounterBuilder b) => b
         ..count = 0
         ..nestedCounter = new NestedCounter((NestedCounterBuilder b) => b..count = 0).toBuilder());
+
+      var middlware = new List<Middleware>();
+      for (int i = 0; i < numMiddleware; i++) {
+        middlware.add(createCounterMiddleware);
+      }
+
       store = new Store(
         defaultValue,
         actions,
-        middleware: [createCounterMiddleware],
+        middleware: [middlware],
       );
-    });
+    }
 
     tearDown(() {
       store.dispose();
     });
 
     test('base action updates state', () async {
+      setup();
       Completer onStateChangeCompleter = new Completer<BaseCounter>();
       store.subscribe.listen((BaseCounter state) => onStateChangeCompleter.complete(state));
       store.actions.increment(2);
@@ -34,6 +41,7 @@ main() {
     });
 
     test('nested action updates state', () async {
+      setup();
       Completer onStateChangeCompleter = new Completer<BaseCounter>();
       store.subscribe.listen((BaseCounter state) => onStateChangeCompleter.complete(state));
       store.actions.nestedCounterActions.increment(2);
@@ -42,11 +50,21 @@ main() {
     });
 
     test('middleware action doubles count and updates state', () async {
+      setup();
       Completer onStateChangeCompleter = new Completer<BaseCounter>();
       store.subscribe.listen((BaseCounter state) => onStateChangeCompleter.complete(state));
       store.actions.middlewareActions.increment(2);
       var nextState = await onStateChangeCompleter.future;
       expect(nextState.count, 4);
+    });
+
+    test('2 middlwares doubles count twice and updates state', () async {
+      setup(numMiddleware: 2);
+      Completer onStateChangeCompleter = new Completer<BaseCounter>();
+      store.subscribe.listen((BaseCounter state) => onStateChangeCompleter.complete(state));
+      store.actions.middlewareActions.increment(2);
+      var nextState = await onStateChangeCompleter.future;
+      expect(nextState.count, 8);
     });
   });
 }
