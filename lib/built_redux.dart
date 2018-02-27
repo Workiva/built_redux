aff267c1b6ba3c180962fcb1680c73cd83bb39c0
Razvan Cristian Lung <lung.razvan@gmail.com>
2018-02-27 19:13:28 +0200

    *export the stream_middleware

--- lib/built_redux.dart
+++ lib/built_redux.dart
@@ -3,6 +3,7 @@ library built_redux;
 export 'src/action.dart';
 export 'src/reducer_builder.dart';
 export 'src/middleware.dart';
+export 'src/stream_middleware.dart';
 export 'src/store.dart';
 export 'src/store_change.dart';
 export 'src/typedefs.dart';
