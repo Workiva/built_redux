8b027b086961f82239904ea2e6d1a4997aaa2cc8
Razvan Cristian Lung <lung.razvan@gmail.com>
2018-02-27 21:03:55 +0200

    add payload to stream action

--- lib/src/stream_middleware.dart
+++ lib/src/stream_middleware.dart
@@ -3,8 +3,17 @@ import 'dart:async';
 import 'package:built_collection/built_collection.dart';
 import 'package:built_redux/built_redux.dart';
 import 'package:built_value/built_value.dart';
+import 'package:meta/meta.dart';
 
 enum StreamAction { subscribe, unsubscribe }
+
+class PayloadAction<P> {
+  PayloadAction({@required this.action, this.payload}) : assert(action != null);
+
+  final StreamAction action;
+
+  final P payload;
+}
 
 /// To use this middleware, in your [ReduxActions] class, add an action with [StreamAction] as the [Action.payload] of
 /// the action. Then you can call that action to subscribe or unsubscribe from the desired stream.
@@ -18,29 +27,29 @@ enum StreamAction { subscribe, unsubscribe }
 ///
 ///     AppActions._();
 ///
-///     ActionDispatcher<SubscriptionAction> get fooStream;
+///     ActionDispatcher<ActionPayload<String>> get fooStream;
 ///  }
 ///  ```
 ///
 /// Them in your lifecycle callback you can:
 /// ```dart
-/// store.actions.fooStream(SubscriptionAction.subscribe);
+/// store.actions.chatsSubscription(new ActionPayload<String>(action: StreamAction.subscribe, payload: 'Some Text'));
 /// ```
 ///
 /// or
 ///
 /// ```dart
-/// store.actions.fooStream(SubscriptionAction.unsubscribe);
+/// store.actions.chatsSubscription(new ActionPayload<Null>(action: StreamAction.unsubscribe));
 /// ```
-///
 class MiddlewareStreamBuilder<State extends Built<State, StateBuilder>, StateBuilder extends Builder<State, StateBuilder>,
     Actions extends ReduxActions> {
   BuiltMap<String, StreamSubscription<dynamic>> _streams = new BuiltMap<String, StreamSubscription<dynamic>>();
 
-  final Map<String, MiddlewareStreamHandler<State, StateBuilder, Actions, dynamic>> _map =
-      <String, MiddlewareStreamHandler<State, StateBuilder, Actions, dynamic>>{};
+  final Map<String, MiddlewareStreamHandler<State, StateBuilder, Actions, dynamic, dynamic>> _map =
+      <String, MiddlewareStreamHandler<State, StateBuilder, Actions, dynamic, dynamic>>{};
 
-  void add<T>(ActionName<T> aMgr, MiddlewareStreamHandler<State, StateBuilder, Actions, T> handler) {
+  void add<Payload, StreamType>(ActionName<PayloadAction<Payload>> aMgr,
+      MiddlewareStreamHandler<State, StateBuilder, Actions, Payload, StreamType> handler) {
     if (!_map.containsKey(aMgr.name)) {
       _map[aMgr.name] = handler;
     }
@@ -59,8 +68,8 @@ class MiddlewareStreamBuilder<State extends Built<State, StateBuilder>, StateBui
             final dynamic subscriptionAction = action.payload;
             final String actionName = action.name;
 
-            if (action.payload is StreamAction) {
-              if (subscriptionAction == StreamAction.unsubscribe) {
+            if (subscriptionAction is PayloadAction) {
+              if (subscriptionAction.action == StreamAction.unsubscribe) {
                 final StreamSubscription<dynamic> streamSubscription = _streams[actionName];
                 streamSubscription.cancel();
 
@@ -69,11 +78,11 @@ class MiddlewareStreamBuilder<State extends Built<State, StateBuilder>, StateBui
                 _streams = (mapBuilder).build();
               } else {
                 if (!_streams.containsKey(actionName)) {
-                  final MiddlewareStreamHandler<State, StateBuilder, Actions, dynamic> handler = _map[actionName];
+                  final MiddlewareStreamHandler<State, StateBuilder, Actions, dynamic, dynamic> handler = _map[actionName];
 
                   final MapBuilder<String, StreamSubscription<dynamic>> mapBuilder = _streams.toBuilder();
 
-                  mapBuilder[actionName] = handler.stream.listen(
+                  mapBuilder[actionName] = handler.getStream(subscriptionAction.payload).listen(
                     (dynamic event) {
                       handler.onData(api, next, action, event);
                     },
@@ -108,11 +117,11 @@ class MiddlewareStreamBuilder<State extends Built<State, StateBuilder>, StateBui
 ///
 /// Example:
 /// ```dart
-/// class ChatHandler extends MiddlewareStreamHandler<AppState, AppStateBuilder, AppActions, Event> {
+/// class ChatHandler extends MiddlewareStreamHandler<AppState, AppStateBuilder, AppActions, String, Event> {
 /// static final Log log = new Log('ChatHandler');
 ///
 /// @override
-/// Stream<Event> get stream => FirebaseDatabase.instance.reference().child('chats');
+/// Stream<Event> getStream(String ref) => FirebaseDatabase.instance.reference().child(ref);
 ///
 /// @override
 /// void onData(MiddlewareApi<AppState, AppStateBuilder, AppActions> api, ActionHandler next, Action action, Event event) {
@@ -128,15 +137,15 @@ class MiddlewareStreamBuilder<State extends Built<State, StateBuilder>, StateBui
 /// }
 /// ```
 abstract class MiddlewareStreamHandler<State extends Built<State, StateBuilder>,
-    StateBuilder extends Builder<State, StateBuilder>, Actions extends ReduxActions, T> {
+    StateBuilder extends Builder<State, StateBuilder>, Actions extends ReduxActions, Payload, StreamType> {
   /// The stream you want to listen for events.
-  Stream<T> get stream;
+  Stream<StreamType> getStream(Payload payload);
 
   /// see [Stream.listen]
   bool cancelOnError = false;
 
   /// This will be trigger every time the stream has new data.
-  void onData(MiddlewareApi<State, StateBuilder, Actions> api, ActionHandler next, Action action, T event);
+  void onData(MiddlewareApi<State, StateBuilder, Actions> api, ActionHandler next, Action action, StreamType event);
 
   /// see [Stream.listen], this callback is optional
   void onDone() {}
