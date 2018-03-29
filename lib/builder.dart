609f85c172c7c7e5277f61ce31a504f6eb127ecf
David Marne <davemarne@gmail.com>
2018-03-28 22:17:42 -0600

    7.4.0-dev

--- lib/builder.dart
+++ lib/builder.dart
@@ -1,10 +1,12 @@
 import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
+import 'package:built_value_generator/built_value_generator.dart';
 
 import 'generator.dart';
 
 Builder builtRedux(BuilderOptions options) => new PartBuilder([
       new BuiltReduxGenerator(),
+      new BuiltValueGenerator(),
     ],
-        generatedExtension: '.built_redux.g.dart',
-        requireLibraryDirective: false);
+        generatedExtension: '.g.dart',
+        header: options.config['header'] as String);
