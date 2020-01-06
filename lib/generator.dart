93cce253f31ae3748a870ff0a0cd37695085c767
Samuel Jackson <samuel.jackson@workiva.com>
2020-01-06 12:24:11 -0600

    Ignores the lint requiring type annotations for public apis

--- lib/generator.dart
+++ lib/generator.dart
@@ -27,6 +27,7 @@ const _lintIgnores = """
 // ignore_for_file: avoid_classes_with_only_static_members
 // ignore_for_file: annotate_overrides
 // ignore_for_file: overridden_fields
+// ignore_for_file: type_annotate_public_apis
 """;
 
 ActionsClass _actionsClassFromElement(ClassElement element) => new ActionsClass(
