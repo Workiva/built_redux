84df80051141744cb1d889f2af0d75cb61758248
Rob Becker <rob.becker@workiva.com>
2023-02-13 09:26:20 -0700

    use github actions for CI

--- example/example.g.dart
+++ example/example.g.dart
@@ -38,10 +38,10 @@ class _$Counter extends Counter {
   final int count;
 
   factory _$Counter([void Function(CounterBuilder)? updates]) =>
-      (new CounterBuilder()..update(updates)).build();
+      (new CounterBuilder()..update(updates))._build();
 
   _$Counter._({required this.count}) : super._() {
-    BuiltValueNullFieldError.checkNotNull(count, 'Counter', 'count');
+    BuiltValueNullFieldError.checkNotNull(count, r'Counter', 'count');
   }
 
   @override
@@ -64,7 +64,7 @@ class _$Counter extends Counter {
 
   @override
   String toString() {
-    return (newBuiltValueToStringHelper('Counter')..add('count', count))
+    return (newBuiltValueToStringHelper(r'Counter')..add('count', count))
         .toString();
   }
 }
@@ -99,14 +99,16 @@ class CounterBuilder implements Builder<Counter, CounterBuilder> {
   }
 
   @override
-  _$Counter build() {
+  Counter build() => _build();
+
+  _$Counter _build() {
     final _$result = _$v ??
         new _$Counter._(
             count: BuiltValueNullFieldError.checkNotNull(
-                count, 'Counter', 'count'));
+                count, r'Counter', 'count'));
     replace(_$result);
     return _$result;
   }
 }
 
-// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,lines_longer_than_80_chars,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new
+// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,deprecated_member_use_from_same_package,lines_longer_than_80_chars,no_leading_underscores_for_local_identifiers,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new,unnecessary_lambdas
--- test/unit/inheritance_test_models.g.dart
+++ test/unit/inheritance_test_models.g.dart
@@ -72,17 +72,17 @@ class _$Child extends Child {
   final int grandparentCount;
 
   factory _$Child([void Function(ChildBuilder)? updates]) =>
-      (new ChildBuilder()..update(updates)).build();
+      (new ChildBuilder()..update(updates))._build();
 
   _$Child._(
       {required this.childCount,
       required this.parentCount,
       required this.grandparentCount})
       : super._() {
-    BuiltValueNullFieldError.checkNotNull(childCount, 'Child', 'childCount');
-    BuiltValueNullFieldError.checkNotNull(parentCount, 'Child', 'parentCount');
+    BuiltValueNullFieldError.checkNotNull(childCount, r'Child', 'childCount');
+    BuiltValueNullFieldError.checkNotNull(parentCount, r'Child', 'parentCount');
     BuiltValueNullFieldError.checkNotNull(
-        grandparentCount, 'Child', 'grandparentCount');
+        grandparentCount, r'Child', 'grandparentCount');
   }
 
   @override
@@ -109,7 +109,7 @@ class _$Child extends Child {
 
   @override
   String toString() {
-    return (newBuiltValueToStringHelper('Child')
+    return (newBuiltValueToStringHelper(r'Child')
           ..add('childCount', childCount)
           ..add('parentCount', parentCount)
           ..add('grandparentCount', grandparentCount))
@@ -161,18 +161,20 @@ class ChildBuilder
   }
 
   @override
-  _$Child build() {
+  Child build() => _build();
+
+  _$Child _build() {
     final _$result = _$v ??
         new _$Child._(
             childCount: BuiltValueNullFieldError.checkNotNull(
-                childCount, 'Child', 'childCount'),
+                childCount, r'Child', 'childCount'),
             parentCount: BuiltValueNullFieldError.checkNotNull(
-                parentCount, 'Child', 'parentCount'),
+                parentCount, r'Child', 'parentCount'),
             grandparentCount: BuiltValueNullFieldError.checkNotNull(
-                grandparentCount, 'Child', 'grandparentCount'));
+                grandparentCount, r'Child', 'grandparentCount'));
     replace(_$result);
     return _$result;
   }
 }
 
-// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,lines_longer_than_80_chars,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new
+// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,deprecated_member_use_from_same_package,lines_longer_than_80_chars,no_leading_underscores_for_local_identifiers,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new,unnecessary_lambdas
